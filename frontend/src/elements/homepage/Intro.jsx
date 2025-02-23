0dfcf352224a10c5277cd8e422dbe268870abe9e
nguyennb28 <nguyennb.coding@gmail.com>
2025-02-23 23:29:50 +0700

    continue make homepage

--- frontend/src/elements/homepage/Intro.jsx
+++ frontend/src/elements/homepage/Intro.jsx
@@ -1,7 +1,11 @@
 import SectionImageRight from "../../components/SectionImageRight";
 
 const IntroHomePage = ({ img, content }) => {
-  return <SectionImageRight image={img} content={content} />;
+  return (
+    <>
+      <SectionImageRight image={img} content={content} />
+    </>
+  );
 };
 
 export default IntroHomePage;
