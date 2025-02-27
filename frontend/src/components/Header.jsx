0c3b35dc46dc10507c010be6837299e8b5063c76
nguyennb28 <nguyennb.coding@gmail.com>
2025-02-27 15:41:10 +0700

    check user logged in hidden button login

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -156,12 +156,28 @@ export default function Example() {
                 </Link>
               </div>
               <div className="py-6">
-                <Link
+                {user ? (
+                  <Link
+                    to="/login"
+                    onClick={logout}
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Đăng xuất
+                  </Link>
+                ) : (
+                  <Link
+                    to="/login"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Đăng nhập <span aria-hidden="true">&rarr;</span>
+                  </Link>
+                )}
+                {/* <Link
                   to="/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Đăng nhập
-                </Link>
+                </Link> */}
               </div>
             </div>
           </div>
