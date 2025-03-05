682427c8b985197cdfbc0083309f37a5d2434c7a
nguyennb28 <nguyennb.coding@gmail.com>
2025-03-05 10:01:23 +0700

    fetch answer_options to questions serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -62,13 +62,15 @@ class ChapterSerializer(serializers.ModelSerializer):
         fields = ["id", "chapter_number", "name", "description", "lessons"]
 
 
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = ["id", "lesson", "queston_text", "question_type"]
-
-
 class AnswerOptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = AnswerOption
         fields = ["id", "question", "answer_text", "is_correct"]
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+
+    answer_options = AnswerOptionSerializer(many=True, read_only=True)
+    class Meta:
+        model = Question
+        fields = ["id", "lesson", "question_text", "question_type", "answer_options"]
